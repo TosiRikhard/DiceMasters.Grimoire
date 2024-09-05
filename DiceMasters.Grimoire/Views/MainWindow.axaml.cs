b6120876b09f55b22c91c7d418008f33b470ba53
Tuomas Savonius <tuomas.savonius@receptum.fi>
2024-09-05 19:04:39 +0300

    Fixed textbox select

--- DiceMasters.Grimoire/Views/MainWindow.axaml.cs
+++ DiceMasters.Grimoire/Views/MainWindow.axaml.cs
@@ -2,6 +2,9 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using DiceMasters.Grimoire.ViewModels;
+using System.Threading.Tasks;
+using Avalonia.VisualTree;
+using System.Linq;
 
 namespace DiceMasters.Grimoire.Views {
     public partial class MainWindow : Window {
@@ -10,12 +13,26 @@ namespace DiceMasters.Grimoire.Views {
             InitializeComponent();
         }
 
-        private void TextBlock_DoubleTapped(object? sender, TappedEventArgs e)
+        private async void TextBlock_DoubleTapped(object? sender, TappedEventArgs e)
         {
             if (sender is TextBlock textBlock &&
                 textBlock.DataContext is AreaViewModel areaViewModel)
             {
                 areaViewModel.IsEditing = true;
+
+                // Wait for the UI to update
+                await Task.Delay(10);
+
+                // Find the TextBox in the same Grid as the TextBlock
+                if (textBlock.Parent is Grid grid)
+                {
+                    var textBox = grid.Children.OfType<TextBox>().FirstOrDefault();
+                    if (textBox != null)
+                    {
+                        textBox.Focus();
+                        textBox.CaretIndex = textBox.Text?.Length ?? 0;
+                    }
+                }
             }
         }
 
@@ -27,14 +44,14 @@ namespace DiceMasters.Grimoire.Views {
                 if (e.Key == Key.Enter)
                 {
                     areaViewModel.IsEditing = false;
-                    e.Handled               = true;
+                    e.Handled = true;
                 }
                 else if (e.Key == Key.Escape)
                 {
                     areaViewModel.IsEditing = false;
                     // Revert the name change
                     textBox.Text = areaViewModel.Name;
-                    e.Handled    = true;
+                    e.Handled = true;
                 }
             }
         }
